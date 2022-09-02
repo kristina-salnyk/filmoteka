eb9be4196f94be3980770b9f0ba7d18b112b5361
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-02 21:38:08 +0200

    Added header layout and style

---
+++ src/js/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '3db99de6ff3e5f2ef6de763ae547f586';
+
+export default axios.create({
+  baseURL: BASE_URL,
+  params: {
+    key: API_KEY,
+  },
+});
