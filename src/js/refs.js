4ab82dcb843d17874deff829cbcf976dcefa3026
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-04 20:37:01 +0200

    Added checking of search query

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 export default {
   searchForm: document.getElementById('search-form'),
+  searchErrorInfo: document.getElementById('search-error-info'),
   watchedBtn: document.getElementById('watched-btn'),
   queueBtn: document.getElementById('queue-btn'),
 };
--- src/js/searchFormSubmitHandler.js
+++ src/js/searchFormSubmitHandler.js
@@ -2,4 +2,13 @@ import refs from './refs';
 
 export const searchFormSubmitHandler = event => {
   event.preventDefault();
+
+  refs.searchErrorInfo.style.display = 'none';
+
+  const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+
+  if (!searchQuery) {
+    refs.searchErrorInfo.style.display = 'block';
+    return;
+  }
 };
