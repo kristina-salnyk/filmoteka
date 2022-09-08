105e4c2475243fe9f5d84f6b883e35702dd48e66
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-08 21:43:49 +0200

    Added btns handlers

--- src/js/homeGalleryClickHandler.js
+++ src/js/homeGalleryClickHandler.js
@@ -1,4 +1,53 @@
-const homeGalleryClickHandler = (e)=>{
+import localStorageService from './local-storage-service';
+import keys from './local-storage-keys';
 
-console.log(e.target)
-}
+const WATCHED_BTN_ID = 'add-to-watched-btn';
+const QUEUE_BTN_ID = 'add-to-queue-btn';
+
+const watchedBtnClickHandler = event => {
+  const movieId = event.target.dataset.id;
+
+  const watchedMoviesIds = localStorageService.load(keys.WATCHED_MOVIES);
+
+  const newWatchedMoviesIds = watchedMoviesIds ? [...watchedMoviesIds] : [];
+
+  if (newWatchedMoviesIds.includes(movieId)) {
+    newWatchedMoviesIds.splice(newWatchedMoviesIds.indexOf(movieId), 1);
+    event.target.classList.remove('tabs__btn--current');
+  } else {
+    newWatchedMoviesIds.push(movieId);
+    event.target.classList.add('tabs__btn--current');
+  }
+
+  localStorageService.save(keys.WATCHED_MOVIES, newWatchedMoviesIds);
+};
+
+const queueBtnClickHandler = event => {
+  const movieId = event.target.dataset.id;
+
+  const queueMoviesIds = localStorageService.load(keys.QUEUE_MOVIES);
+
+  const newQueueMoviesIds = queueMoviesIds ? [...queueMoviesIds] : [];
+
+  if (newQueueMoviesIds.includes(movieId)) {
+    newQueueMoviesIds.splice(newQueueMoviesIds.indexOf(movieId), 1);
+    event.target.classList.remove('tabs__btn--current');
+  } else {
+    newQueueMoviesIds.push(movieId);
+    event.target.classList.add('tabs__btn--current');
+  }
+
+  localStorageService.save(keys.QUEUE_MOVIES, newQueueMoviesIds);
+};
+
+export const homeGalleryClickHandler = event => {
+  event.preventDefault();
+
+  if (event.target.id === WATCHED_BTN_ID) {
+    watchedBtnClickHandler(event);
+  }
+
+  if (event.target.id === QUEUE_BTN_ID) {
+    queueBtnClickHandler(event);
+  }
+};
--- src/js/local-storage-service.js
+++ src/js/local-storage-service.js
@@ -1,27 +1,15 @@
 const save = (key, value) => {
-  try {
-    const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
-  } catch (error) {
-    // console.error('Set state error: ', error.message);
-  }
+  const serializedState = JSON.stringify(value);
+  localStorage.setItem(key, serializedState);
 };
 
 const load = key => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    // console.error('Get state error: ', error.message);
-  }
+  const serializedState = localStorage.getItem(key);
+  return serializedState === null ? undefined : JSON.parse(serializedState);
 };
 
 const remove = key => {
-  try {
-    localStorage.removeItem(key);
-  } catch (error) {
-    // console.error('Remove state error: ', error.message);
-  }
+  localStorage.removeItem(key);
 };
 
 export default {
