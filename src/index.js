243e1db80fe0bae7a1c07bcd6f464da595adef80
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-08 18:11:06 +0200

    Resolved conflict

--- src/index.js
+++ src/index.js
@@ -1,44 +1,22 @@
 import ui from './js/ui-interactions';
-import './js/pagination';
 import notifications from './js/notifications';
 import MovieService from './js/MovieService';
+import { loadTrendingMovies } from './js/loadTrendingMovies';
+import './js/pagination';
+
+export const movieService = new MovieService();
 
 ui.setHomeEventListeners();
 
- const movieService = new MovieService();
-
-loadTrendingMovies().catch(error => {
+initHomePage().catch(error => {
   notifications.failedRequest();
   console.log(error);
   // TODO: handle error of fetching movies in home page
 });
 
- async function loadTrendingMovies() {
+async function initHomePage() {
   await movieService.fetchGenresList();
-  const data = await movieService.fetchTrendingMovies();
-  
-  const {
-    results: movies,
-    total_pages: totalPages,
-    total_results: totalResults,
-  } = data;
-  
-  if (totalResults === 0) {
-    notifications.notFoundResults();
-    return;
-  }
-  
-  const moviesData = movies.map(item => {
-    const newItem = { ...item };
-    newItem.genres = item.genre_ids
-    .map(id => movieService.getGenreById(id))
-    .join(', ');
-    const releaseDate = new Date(item.release_date);
-    newItem.year = releaseDate.getFullYear();
-    return newItem;
-  });
-  
-  ui.appendGalleryMarkup(moviesData);
+  return loadTrendingMovies();
 }
 
 // movieService.searchQuery = 'Time';
