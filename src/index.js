8b3b937dfa06e666160a4a2a19d41caf0feacc23
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-09-08 18:17:50 +0300

    Pagination/moviesGet

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import ui from './js/ui-interactions';
+import './js/pagination';
 import notifications from './js/notifications';
 import MovieService from './js/MovieService';
 
 ui.setHomeEventListeners();
 
-const movieService = new MovieService();
+ const movieService = new MovieService();
 
 loadTrendingMovies().catch(error => {
   notifications.failedRequest();
@@ -12,31 +13,31 @@ loadTrendingMovies().catch(error => {
   // TODO: handle error of fetching movies in home page
 });
 
-async function loadTrendingMovies() {
+ async function loadTrendingMovies() {
   await movieService.fetchGenresList();
   const data = await movieService.fetchTrendingMovies();
-
+  
   const {
     results: movies,
     total_pages: totalPages,
     total_results: totalResults,
   } = data;
-
+  
   if (totalResults === 0) {
     notifications.notFoundResults();
     return;
   }
-
+  
   const moviesData = movies.map(item => {
     const newItem = { ...item };
     newItem.genres = item.genre_ids
-      .map(id => movieService.getGenreById(id))
-      .join(', ');
+    .map(id => movieService.getGenreById(id))
+    .join(', ');
     const releaseDate = new Date(item.release_date);
     newItem.year = releaseDate.getFullYear();
     return newItem;
   });
-
+  
   ui.appendGalleryMarkup(moviesData);
 }
 
---
+++ src/js/pagination.js
@@ -0,0 +1,80 @@
+import ui from './ui-interactions';
+import refs from './refs';
+import MovieService from './MovieService';
+
+const pagin = document.querySelector('.pagination');
+const paginList = document.querySelector('.pagination__list');
+const list = document.querySelectorAll('.link');
+const rightArrow = document.getElementById('rightArrow');
+const leftArrow = document.getElementById('leftArrow');
+
+// console.log(rightArrow);
+// console.log(leftArrow);
+// console.log(pagin);
+// console.log(paginList);
+list.forEach(element => {
+
+});
+const movieService = new MovieService();
+// const moveList = movieService.fetchTrendingMovies()
+// moveList.then(console.log)
+
+pagin.addEventListener('click', getNewPage);
+
+export function getNewPage(e) {
+     e.preventDefault();
+    if (e.target.nodeName !== 'A') {
+      return;
+    }
+    console.log(e.target.classList.contains('left__arrow'));
+   if (e.target.classList.contains('left__arrow')) {
+     movieService.decrementPage();
+     refs.homeGallery.innerHTML = '';
+     loadMovies();
+   }
+   
+    if (e.target.classList.contains('right__arrow')) {
+        movieService.incrementPage();
+        refs.homeGallery.innerHTML = '';
+        loadMovies();
+       
+    }
+    if (!e.target.classList.contains('right__arrow') && !e.target.classList.contains('left__arrow')) {
+        const page = Number(e.target.innerHTML);
+        movieService.setPage(page);
+         refs.homeGallery.innerHTML = '';
+         loadMovies();
+    }
+    console.log(movieService.getPage())
+    
+}
+
+
+
+
+
+
+
+async function loadMovies() {
+    await movieService.fetchGenresList();
+  const data = await movieService.fetchTrendingMovies();
+//   console.log(movieService.fetchTrendingMovies);
+  const {
+    results: movies,
+    total_pages: totalPages    
+  } = data;
+  
+  
+  
+  const moviesData = movies.map(item => {
+    const newItem = { ...item };
+    newItem.genres = item.genre_ids
+    .map(id => movieService.getGenreById(id))
+    .join(', ');
+    const releaseDate = new Date(item.release_date);
+    newItem.year = releaseDate.getFullYear();
+    return newItem;
+  });
+  
+  ui.appendGalleryMarkup(moviesData);
+}
