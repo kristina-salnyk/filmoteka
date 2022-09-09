3bd42c354a350299b191d57c9ddcb5ba61aa7295
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-10 00:39:43 +0200

    Fixed scroll

--- src/js/home-page-ui.js
+++ src/js/home-page-ui.js
@@ -2,12 +2,15 @@ import refs from './refs';
 import { searchFormSubmitHandler } from './searchFormSubmitHandler';
 import { oneMovieClickHandler } from './oneMovieClickHandler';
 import { dynamicRefs } from './dynamicRefs';
+import { trackScroll, backToTop } from './scroll-to-up-btn';
 import movieCards from '../templates/movie-card.hbs';
 import modalMovie from '../templates/modal-for-movie.hbs';
 
 const setHomeEventListeners = () => {
   refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
   refs.homeGallery.addEventListener('click', oneMovieClickHandler);
+  window.addEventListener('scroll', trackScroll);
+  refs.goTopBtn.addEventListener('click', backToTop);
 };
 
 const appendGalleryMarkup = movies => {
--- src/js/scroll-to-up-btn.js
+++ src/js/scroll-to-up-btn.js
@@ -1,24 +1,19 @@
-// import refs from './refs';
+import refs from './refs';
 
-const goTopBtn = document.querySelector('.back-to-top')
-
-const trackScroll = () => {
+export const trackScroll = () => {
   const scrolled = window.pageYOffset;
   const coords = document.documentElement.clientHeight;
 
   if (scrolled > coords) {
-    goTopBtn.classList.add('back-to-top_show');
+    refs.goTopBtn.classList.add('back-to-top_show');
   }
   if (scrolled < coords) {
-    goTopBtn.classList.remove('back-to-top_show');
+    refs.goTopBtn.classList.remove('back-to-top_show');
   }
 };
 
-const backToTop = () => {
+export const backToTop = () => {
   if (window.pageYOffset > 0) {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 };
-
-window.addEventListener('scroll', trackScroll);
-goTopBtn.addEventListener('click', backToTop);
