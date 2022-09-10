8069e2779def708c60c695e066201957c958604d
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-10 20:02:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import MovieService from './js/MovieService';
 import homePageUi from './js/ui/home-page-ui';
 import notifications from './js/notifications';
 import { loadTrendingMovies } from './js/loadTrendingMovies';
-import './js/footer-modal';
+// import './js/footer-modal';
 import Swiper, { Navigation, Pagination } from 'swiper';
 // import 'swiper/css';
 // import 'swiper/css/navigation';
