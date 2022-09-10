398a6a53e854a1cae26101f6dff8e9b4f859e82e
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2022-09-10 11:05:11 +0300

    add

--- src/js/modalBtnsClickHandler.js
+++ src/js/modalBtnsClickHandler.js
@@ -1,31 +1,25 @@
 import localStorageService from './local-storage-service';
 import keys from './local-storage-keys';
 
-
-const WATCHED_BTN_ID = 'watched-btn';
-const QUEUE_BTN_ID = 'queue-btn';
-const watchedBtnClickHandler = event => {
+export const modalWatchedBtnClickHandler = event => {
   const movieId = event.target.dataset.id;
-  
+
   const watchedMoviesIds = localStorageService.load(keys.WATCHED_MOVIES);
 
   const newWatchedMoviesIds = watchedMoviesIds ? [...watchedMoviesIds] : [];
- 
+
   if (newWatchedMoviesIds.includes(movieId)) {
     newWatchedMoviesIds.splice(newWatchedMoviesIds.indexOf(movieId), 1);
     event.target.classList.remove('tabs__btn--current');
-    
-    
   } else {
     newWatchedMoviesIds.push(movieId);
     event.target.classList.add('tabs__btn--current');
-
   }
 
   localStorageService.save(keys.WATCHED_MOVIES, newWatchedMoviesIds);
 };
 
-const queueBtnClickHandler = event => {
+export const modalQueueBtnClickHandler = event => {
   const movieId = event.target.dataset.id;
 
   const queueMoviesIds = localStorageService.load(keys.QUEUE_MOVIES);
@@ -35,30 +29,10 @@ const queueBtnClickHandler = event => {
   if (newQueueMoviesIds.includes(movieId)) {
     newQueueMoviesIds.splice(newQueueMoviesIds.indexOf(movieId), 1);
     event.target.classList.remove('tabs__btn--current');
-   
   } else {
     newQueueMoviesIds.push(movieId);
     event.target.classList.add('tabs__btn--current');
   }
 
   localStorageService.save(keys.QUEUE_MOVIES, newQueueMoviesIds);
-};
-
-export const modalWatchedBtnClickHandler = event => {
-  event.preventDefault();
-
-  if (event.target.id === WATCHED_BTN_ID) {
-    watchedBtnClickHandler(event);
-  }
-
-  if (event.target.id === QUEUE_BTN_ID) {
-    queueBtnClickHandler(event);
-  }
-};
-
-export const modalQueueBtnClickHandler = event => {
-  event.preventDefault();
-
-
-  
 };
