ee1e1a977ed0e59465f181c41fb0c59d65a2fcf4
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-10 19:04:17 +0200

    Back to old version

--- src/js/loadMoviesPagination.js
+++ src/js/loadMoviesPagination.js
@@ -1,23 +1,24 @@
-import refs from './refs';
-import { movieService } from '../index';
-import homePageUi from './home-page-ui';
-import { dynamicRefs } from './dynamicRefs'
-import paginationMarup from './pagination'
+import refs from './refs/refs';
+import { movieConfigs } from '../index';
+import homePageUi from './ui/home-page-ui';
+import { dynamicRefs } from './refs/dynamicRefs';
+import paginationMarkup from './pagination';
+import { fetchTrendingMovies } from './api/movie-api/fetchTrendingMovies';
 
 
 dynamicRefs().paginList.addEventListener('click', getNewPage);
 dynamicRefs().leftArrow.addEventListener('click', leftBtnClick);
 dynamicRefs().rightArrow.addEventListener('click', rightBtnClick);
 
-   
+
 export default function leftBtnClick() {
-  movieService.decrementPage();
+  movieConfigs.decrementPage();
   refs.homeGallery.innerHTML = '';
   loadMovies();
 }
 
 export default function rightBtnClick() {
-  movieService.incrementPage();
+  movieConfigs.incrementPage();
   refs.homeGallery.innerHTML = '';
   loadMovies();
 }
@@ -31,20 +32,20 @@ export default function getNewPage(e) {
 
   if (e.target.innerHTML !== '...') {
     const page = Number(e.target.innerHTML);
-    movieService.setPage(page);
+    movieConfigs.page = page;
     refs.homeGallery.innerHTML = '';
     loadMovies();
   }
 }
 
 async function loadMovies() {
-  const data = await movieService.fetchTrendingMovies();
+  const data = await fetchTrendingMovies();
   const { results: movies, total_pages: totalPages } = data;
 
   const moviesData = movies.map(item => {
     const newItem = { ...item };
     newItem.genres = item.genre_ids
-      .map(id => movieService.getGenreById(id))
+      .map(id => movieConfigs.getGenreById(id))
       .join(', ');
     const releaseDate = new Date(item.release_date);
     newItem.year = releaseDate.getFullYear();
@@ -53,5 +54,5 @@ async function loadMovies() {
   });
 
   homePageUi.appendGalleryMarkup(moviesData);
-  paginationMarup(totalPages, movieService.getPage());
+  paginationMarkup(totalPages, movieConfigs.page);
 }
