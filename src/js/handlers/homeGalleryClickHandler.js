fe926f52b072decba76ffc66f35f7575111eeeba
maksim stryuts <maxim.stryuts@gmail.com>
2022-09-11 20:58:31 +0300

    .

--- src/js/handlers/homeGalleryClickHandler.js
+++ src/js/handlers/homeGalleryClickHandler.js
@@ -1,6 +1,11 @@
 import { fetchMovieById } from '../api/movie-api/fetchMovieById';
 import { renderOneMovieData } from '../render/renderOneMovieData';
 import notifications from '../notifications';
+// import { Spinner } from 'spin.js';
+// import 'spin.js/spin.css';
+// import spinnerConfigs from '../spinnerCofigs';
+
+// const spinner = new Spinner(spinnerConfigs);
 
 export const homeGalleryClickHandler = async event => {
   event.preventDefault();
@@ -8,7 +13,9 @@ export const homeGalleryClickHandler = async event => {
   const movieId = event.target.closest('a').dataset.id;
 
   try {
+    // spinner.spin(refs.homeGallery);
     const movieData = await fetchMovieById(movieId);
+    // spinner.stop();
     renderOneMovieData(movieData);
   } catch (error) {
     notifications.failedRequest();
