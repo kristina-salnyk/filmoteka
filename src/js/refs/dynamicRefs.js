16a2c131d5dfd1e15b7ccedce57fde555d61c0d4
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-11 08:51:30 +0200

    Fixed attribute name

--- src/js/refs/dynamicRefs.js
+++ src/js/refs/dynamicRefs.js
@@ -1,12 +1,11 @@
 export const dynamicRefs = () => ({
   watchedBtn: document.getElementById('watched-btn'),
   queueBtn: document.getElementById('queue-btn'),
-  movieData: document.querySelector('[movie-data]'),
+  movieData: document.querySelector('[data-movie]'),
   closeModalBtn: document.querySelector('[data-action="close-modal"]'),
   backdrop: document.querySelector('.js-modal-close-btn'),
   btnCloseModalYoutybe: document.querySelector('[data-action="btn__close"]'),
   playTrailer: document.getElementById('play-trailer'),
-
   paginList: document.querySelector('.pagination__list'),
   leftArrow: document.querySelector('[data-arrow="left"]'),
   rightArrow: document.querySelector('[data-arrow="right"]'),
