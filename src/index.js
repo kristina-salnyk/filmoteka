38d2f360b9fc3f151aff2f99b199e8ce53127099
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-11 18:22:06 +0200

    Added spinner init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import MovieConfigs from './js/MovieConfigs';
+import refs from './js/refs/refs';
 import homePageUi from './js/ui/home-page-ui';
 import notifications from './js/notifications';
 import { fetchGenresList } from './js/api/movie-api/fetchGenresList';
 import { fetchTrendingMovies } from './js/api/movie-api/fetchTrendingMovies';
 import { renderMoviesData } from './js/render/renderMoviesData';
- import './js/footer-modal';
-import Swiper, { Navigation, Pagination } from 'swiper';
- import './js/loadPagePagination'
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import './js/footer-modal';
+import './js/loadPagePagination';
+import { Spinner } from 'spin.js';
+import 'spin.js/spin.css';
+import spinnerConfigs from './js/spinnerCofigs';
 
 export const movieConfigs = new MovieConfigs();
+
+const spinner = new Spinner(spinnerConfigs);
 
 initHomePage().catch(error => {
   notifications.failedRequest();
@@ -21,12 +23,16 @@ async function initHomePage() {
   homePageUi.setHomeEventListeners();
 
   try {
+    spinner.spin(refs.homeGallery);
+
     const genresData = await fetchGenresList();
     const { genres } = genresData;
     movieConfigs.genres = genres;
 
     const trendingData = await fetchTrendingMovies();
     renderMoviesData(trendingData);
+
+    spinner.stop();
   } catch (error) {
     notifications.failedRequest();
   }
