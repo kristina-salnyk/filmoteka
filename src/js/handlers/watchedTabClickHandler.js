ffaec0c04d13d3385cfe19a8d6050fc61c6fbb5e
OlyaTrush <olya.trush@gmail.com>
2022-09-11 18:01:33 +0300

    last changes

--- src/js/handlers/watchedTabClickHandler.js
+++ src/js/handlers/watchedTabClickHandler.js
@@ -12,7 +12,38 @@ export const watchedTabClickHandler = event => {
   refs.libraryGallery.innerHTML = '';
   const watchedMovieIds = storage.load(key.WATCHED_MOVIES);
   console.log(watchedMovieIds);
- 
-
+    processMovieIds(watchedMovieIds).then(data => {
+      const test = {
+        results: data
+      };
+      renderLibraryMoviesData(test);
+    });
+    
+    
 };
 
+const processMovieIds = async ( ids )=> {
+  const movieRequests = ids.map(id => fetchMovieById(id));
+  const result = await Promise.all(movieRequests);
+  return result;
+};
+const renderLibraryMoviesData = data => {
+  const {
+    results: movies,
+    total_pages: totalPages,
+    total_results: totalResults,
+  } = data;
+
+  paginationMarkup(totalPages, libraryMovieConfigs.page);
+
+  const moviesData = movies.map(item => {
+    const newItem = { ...item };
+    newItem.genres = item.genres.map(genre => genre.name).join(', ');
+    const releaseDate = new Date(item['release_date']);
+    newItem.year = releaseDate.getFullYear();
+    newItem.vote = item['vote_average'].toFixed(1);
+    return newItem;
+  });
+
+  libraryPageUi.appendGalleryMarkup(moviesData);
+};
