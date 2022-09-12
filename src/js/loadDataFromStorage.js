d521736e0fde98f534f8bdf7ecf574154d9f2523
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-12 17:47:27 +0200

    Deleted console log

--- src/js/loadDataFromStorage.js
+++ src/js/loadDataFromStorage.js
@@ -2,7 +2,6 @@ import { auth, getDataFromStorage } from './api/firebase/fireBase';
 import storage from './local-storage/local-storage-service';
 
 export const loadDataFromStorage = async key => {
-  console.log(auth.currentUser);
   if (auth.currentUser) {
     return await getDataFromStorage(key);
   } else {
