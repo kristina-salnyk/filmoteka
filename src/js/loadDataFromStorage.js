69aa81248f8ed1dc9e2c5dbac3f7b250148aefa9
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-09-13 12:58:48 +0200

    Update loadDataFromStorage.js

--- src/js/loadDataFromStorage.js
+++ src/js/loadDataFromStorage.js
@@ -1,5 +1,6 @@
-import { auth, getDataFromStorage } from './api/firebase/fire-base-service';
+import { auth } from './api/firebase/fire-base-service';
 import storage from './local-storage/local-storage-service';
+import { getDataFromStorage } from './api/firebase/getDataFromStorage';
 
 export const loadDataFromStorage = async key => {
   if (auth.currentUser) {
